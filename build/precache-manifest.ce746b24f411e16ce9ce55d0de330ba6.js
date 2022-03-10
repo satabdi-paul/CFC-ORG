08527f9ec7038c2aae311f4d6f5e2dbc3d80f95a
satabdi paul <44485003+satabdi-paul@users.noreply.github.com>
2022-03-10 21:14:50 +0530

    Add files via upload

---
+++ build/precache-manifest.ce746b24f411e16ce9ce55d0de330ba6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6df70a5b00d57e894502d24bf6776356",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d286fb8ed6f803dfd4b9",
+    "url": "/static/css/2.aec09b9b.chunk.css"
+  },
+  {
+    "revision": "9eee83b572406fb6c971",
+    "url": "/static/css/main.d4c0d097.chunk.css"
+  },
+  {
+    "revision": "d286fb8ed6f803dfd4b9",
+    "url": "/static/js/2.9065a98b.chunk.js"
+  },
+  {
+    "revision": "42d40ab3484f9d3e31aeb4e608c1591a",
+    "url": "/static/js/2.9065a98b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9eee83b572406fb6c971",
+    "url": "/static/js/main.3ca6796e.chunk.js"
+  },
+  {
+    "revision": "b05506d192fa13a4b14c",
+    "url": "/static/js/runtime-main.cb08f1e8.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
